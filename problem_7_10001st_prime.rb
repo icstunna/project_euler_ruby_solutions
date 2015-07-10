ada8c078dbc6c438c3d6c45f3d069f2681bd50bf
icstunna <mdelostr@gmail.com>
2015-07-09 22:32:58 -0700

    created 2 methods, one to find the next prime number the other to determine what order that number is in the prime order. the algorithm is good up to the 8000th prime number but obtains a stack level too deep issue at the 10000th prime number.

---
+++ problem_7_10001st_prime.rb
@@ -0,0 +1,27 @@
+#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+#What is the 10 001st prime number?
+require "Prime"
+
+def next_prime(int)
+  int = int + 1
+  if int.prime?
+    return int
+  else
+    return next_prime(int)
+  end
+end
+
+def nth_prime(int, n, count = 0)
+  int = next_prime(int)
+  count = count
+  if count == n
+    return int
+  else
+    count += 1
+    nth_prime(int, n, count)
+  end
+end
+
+# p next_prime(1000)
+p nth_prime(1, 8000)
